78af7ae154b31598bb672beeb1fe66a127564966
kevin <pkevin2007@hotmail.com>
2017-10-09 22:32:07 -0500

    -

--- destino/main.js
+++ destino/main.js
@@ -1,3 +1,27 @@
 "use strict";
 
-var inicio = "hola";
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Dog = function () {
+  function Dog(age) {
+    _classCallCheck(this, Dog);
+
+    this.age = age;
+  }
+
+  _createClass(Dog, [{
+    key: "getHumanage",
+    value: function getHumanage() {
+
+      return this.age * 7;
+    }
+  }]);
+
+  return Dog;
+}();
+
+var Stanley = new Dog(10);
+
+console.log(Stanley.getHumanage());
--- fuente/main.js
+++ fuente/main.js
@@ -1,1 +1,20 @@
-let inicio = "hola";
+class Dog{
+
+
+ constructor(age) {
+
+   this.age = age;
+
+ }
+
+ getHumanage() {
+
+   return (this.age * 7);
+
+ }
+
+}
+
+const Stanley = new Dog(10);
+
+console.log(Stanley.getHumanage());
